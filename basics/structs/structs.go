954de9189750de1b6ae577b807dd843552af3ab2
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2024-03-16 13:03:33 -0400

    finished sections 5 and 6

--- basics/structs/structs.go
+++ basics/structs/structs.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"fmt"
+	"structs/user"
 )
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	birthDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	// ... do something awesome
+	appUser, err := user.New(firstName, lastName, birthDate)
 
-	fmt.Println(firstName, lastName, birthdate)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	admin := user.NewAdmin("test@example.com", "test123")
+	admin.OutputUserDetails()
+	admin.ClearUserName()
+	admin.OutputUserDetails()
+
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
